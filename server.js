8cf69ab5d6a7359f2512893bb4365243d9231a2d
Nada Smith <ayronadas@gmail.com>
2023-08-21 22:43:02 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -11,17 +11,32 @@ app.get('/getting', (req, res) => {
     res.send('Hello, stranger');
 });
 
-//route handling GET requests with a route parameter
+//route handling GET requests with name parameter
 app.get('/greeting/:name', (req, res) => {
     //retrieves the captured parameter value
     const name = req.params.name;
     //greeting message with captured name
     const greeting = `What's up, ${name}!`;
+    //sends greeting back to the client
     res.send(greeting)   ;
 })
 
+//route handling GET requests with total and tipPercentage parameter
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    //retrieves the captured parameter value; parseFloat used to convert values to floating-point numbers bc they will be received as strings in URL
+    const total = parseFloat(req.params.total);
+    const tipPercentage = parseFloat(req.params.tipPercentage);
+    //calculates tip amount based on the param values; 
+    const totalTip = (total * tipPercentage) / 100;
+    //sends the tip amount back to the client
+    res.send(`${totalTip}`);
+})
 
 //starts server and listens to port
 app.listen(port, () => {
     console.log(`Server is running no port ${port}`);
 });
+
+
+
+/* 'req.params' contains anobject with key-value pairs where the keys are the parameter names and the values are the valuese provided in the URL. */
